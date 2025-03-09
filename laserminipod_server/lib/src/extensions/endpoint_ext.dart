abe740b28cf7594be4974d5d4d77ef7914af5c88
MrKaktusFilm <marko.wy@gmx.de>
2025-03-09 11:40:09 +0100

    fix scope check serverside

--- laserminipod_server/lib/src/extensions/endpoint_ext.dart
+++ laserminipod_server/lib/src/extensions/endpoint_ext.dart
@@ -1,20 +1,28 @@
 import 'package:serverpod/serverpod.dart';
 
 extension Validation on Endpoint {
-  Future<void> _validateScope(Session session, String requiredScope) async {
-    // TODO: fix
+  Future<void> _validateScope(
+      Session session, List<String> requiredScope) async {
     var userInfo = await session.authenticated;
-    if (userInfo == null ||
-        !userInfo.scopes.map((scope) => scope.name).contains(requiredScope)) {
-      throw Exception('Unauthorized: scope $requiredScope is needed');
+
+    var hasAnyRequiredScope = false;
+    for (var scope in requiredScope) {
+      if (userInfo!.scopes.map((s) => s.name).contains(scope)) {
+        hasAnyRequiredScope = true;
+        break;
+      }
+    }
+    if (!hasAnyRequiredScope) {
+      throw Exception(
+          'Unauthorized: one of these scopes is needed: ${requiredScope.join(", ")}');
     }
   }
 
   Future<void> validateUserScope(Session session) async {
-    await _validateScope(session, 'user');
+    await _validateScope(session, ['user', 'admin']);
   }
 
   Future<void> validateAdminScope(Session session) async {
-    await _validateScope(session, 'admin');
+    await _validateScope(session, ['admin']);
   }
 }
